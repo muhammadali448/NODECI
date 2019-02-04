6a3199d928d0cbfbcf4525b4232c842c51cb25b6
muhammadali448 <muhammadali19960@outlook.com>
2019-02-04 14:43:48 +0500

    all push 2

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -7,7 +7,8 @@ class CustomPage {
   }
   static async build() {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
+      args: ['--no-sandbox']
     });
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
