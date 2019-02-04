40f3c5c8152b7c9db60cd39c43aa3b5e022d3dcf
muhammadali448 <muhammadali19960@outlook.com>
2019-02-04 15:32:34 +0500

    This is a new commit for what I originally planned to be amended

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://127.0.0.1:27018',
+  mongoURI: 'mongodb://ali448:hideit90@ds121105.mlab.com:21105/blogs',
   redisUrl: 'redis://127.0.0.1:6379',
   cookieKey: '123123123'
 };
